71ff12930244ac20480d743a19ed969b51c215f0
Mike Kress <mkress@taos.com>
2020-06-13 07:39:25 -0600

    Update decorators_stuff.py

--- python/decorators_stuff.py
+++ python/decorators_stuff.py
@@ -8,7 +8,11 @@ def announce(method):
     @wraps(method)
     def wrapper(instance, *args, **kwargs) -> object:
         print(f'Running {method.__name__}, {method.__doc__}')
-        method(instance, *args, **kwargs)
+        resp = method(instance, *args, **kwargs)
+        try:
+            print(f'Response {resp["message"]}')
+        except TypeError:
+            print(resp)
     return wrapper
 
 
@@ -47,28 +51,38 @@ class Thing:
 
     @security
     @announce
-    def bigger(self, *args) -> int:
+    def bigger(self, *args) -> dict:
         """This makes a thing bigger!"""
         _old_size = self.size
         self.size = args[0]
         message = f'The {self} has changed in size by {self.size - _old_size}'
-        print(message)
-        return self.size
+        return dict(size=self.size, message=message)
 
     @security
     @announce
-    def flip_polarity(self) -> bool:
+    def flip_polarity(self) -> dict:
         """This will reverse the current polarity, either true or false, whichever is opposite."""
         _old = self.polarity
         self.polarity = not self.polarity
         message = f'The polarity was changed from {_old} to {self.polarity}'
-        print(message)
-        return self.polarity
+        return dict(size=self.polarity, message=message)
+
+
+    @announce
+    def blocking_calls(self):
+        """Lets say we have to make long costly blocking calls to an api, that we can yield one by one"""
+        from time import sleep
+        for i in range(10):
+            sleep(2.875)
+            yield random.random(1, 10000000000)
+
 
 
 class DerivedThing(Thing):
-    """Lets modify the class by adding some additional attrs, and perhaps a method, using inheritance but also make
-    sure that the original class supports the methods we expect it to."""
+    """
+    Lets modify the class by adding some additional attrs, and perhaps a method, using inheritance but also make
+    sure that the original class supports the methods we expect it to.
+    """
     state: dict
 
     def __init_subclass__(cls, **kwargs):
@@ -83,6 +97,7 @@ def main():
     for i in range(12):
         thing.bigger(rando(1, 100))
         thing.flip_polarity()
+        thing.blocking_calls()
 
 
 if __name__ == '__main__':
