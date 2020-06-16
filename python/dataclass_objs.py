d72db03f0e8e40e4f4f01fccc6dadc151bdef40f
Mike Kress <mkress@taos.com>
2020-06-16 08:41:20 -0600

    Create dataclass_objs.py
    learned about dataclass()

---
+++ python/dataclass_objs.py
@@ -0,0 +1,32 @@
+"""
+From Raymond Hettingers talk : Dataclasses: The code generator to end all code generators - PyCon 2018
+
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Color:
+    x: int
+    y: float
+    z: float = .05
+
+
+x = 2
+y = 3.7
+
+data = {
+    'colors': [
+        {'x': x, 'y': y},
+        {'x': 1, 'y': 6.337},
+    ]
+}
+
+c = Color(5, 2.7)
+
+colors = [Color(**color) for color in data['colors']]
+
+for i in colors:
+    print(i)
+
